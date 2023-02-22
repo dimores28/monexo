78e9bddb7d7463fd77b417c7a0f3ab0ee9cee414
Dmytro <dimores95@gmail.com>
2023-02-22 15:15:52 +0200

    added about screan and page scroll

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 import './main.less'
+import $ from 'jquery';
+import  './src/js/jquery.pagepiling.min.js';
+
+window.$ = $;
+
 
  document.querySelector('#burger-btn').addEventListener('click', function(){
      let nav = document.querySelector('.mobile-navigatiion');
      nav.classList.toggle('show');
 });
+
+
+$('#pagepiling').pagepiling();
 
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import legacy from '@vitejs/plugin-legacy'
 import { defineConfig } from 'vite'
+import inject from "@rollup/plugin-inject";
 
 export default defineConfig({
   plugins: [
     legacy({
       targets: ['defaults', 'not IE 11'],
+    }),
+    inject({   // => that should be first under plugins array
+        $: 'jquery',
+        jQuery: 'jquery',
     }),
   ],
 })
