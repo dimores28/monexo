5213be7308c27cc537eca8489fe74c3c87108c1a
Dmytro <dimores95@gmail.com>
2023-02-23 16:37:55 +0200

    create pages reviews and provide

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './main.less'
 import $ from 'jquery';
 import  './src/js/jquery.pagepiling.min.js';
+import './src/js/slick.min.js'
 
 window.$ = $;
 
@@ -11,5 +12,43 @@ window.$ = $;
 });
 
 
-$('#pagepiling').pagepiling();
+$('#pagepiling').pagepiling({
+    anchors: ['home-page', 'about', 'offers',  'fourthPage', 'lastPage'],
+    menu: '#menu',
+    // normalScrollElements: '#offers, .element2'
+});
+
+$('#reviews-slider').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 
